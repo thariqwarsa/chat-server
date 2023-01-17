743a8922c6094319575e5e2899d672042aa073dc
Thariq Warsahemas <thariqwarsahemas@gmail.com>
2023-01-17 14:02:31 +0700

    [ADD] cors

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
 });
+
+const port = process.env.PORT || 5000;
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 io.on('connection', (socket) => {
   io.emit('server_send_message', 'server: someone connected!')
@@ -15,7 +21,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    io.emit('server_send_message', `server: a user disconnected`)
+    io.emit('server_send_message', `a user disconnected`)
   })
 });
 
