ce1b32d02b4ef237fe6b4ba771b18ab49f57fee4
Thariq Warsahemas <thariqwarsahemas@gmail.com>
2023-01-27 09:08:48 +0700

    [ADD] agreed event names

--- index.js
+++ index.js
@@ -14,17 +14,61 @@ const port = process.env.PORT || 5000;
 // });
 
 io.on('connection', (socket) => {
-  io.emit('server_send_message', 'server: someone connected!')
+  socket.on('create_ticket', msg => {
+    console.log('create_ticket')
+    // {
+    //   response: 'success|error'
+    //   error_message: null,
+    //   data: {
+    //     ticket_id: 'x'
+    //   }
+    // }
+  });
 
-  socket.on('user_send_message', msg => {
-    io.emit('user_send_message', `user: ${msg}`)
+  socket.on('send_message', msg => {
+    console.log('send_message')
+    // {
+    //   response: 'success|error'
+    //   error_message: null,
+    //   data: {
+    //     message_id: 'x', 
+    //     status: 'delivered|failed'
+    //   }
+    // }
+  });
+
+  socket.emit('receive_message', msg => {
+    console.log('receive_message')
+  });
+
+  socket.on('take_tikcet', msg => {
+    console.log('take_tikcet')
+    // {
+    //   response: 'success|error'
+    //   error_message: null,
+    //   data: {
+    //     ticket_id: 'xx',
+    //     status: 'in_progress',
+    //     agent_id: 'xx'
+    //   }
+    // }
+  });
+
+  socket.on('ticket_updated', msg => {
+    console.log('ticket_updated')
   });
 
   socket.on('disconnect', () => {
-    io.emit('server_send_message', `a user disconnected`)
+    io.emit('server_log', `a user disconnected`)
   })
 });
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
+
+// {
+//   response: 'success|error'
+//   error_message: 'something wrong',
+//   data: null
+// }
